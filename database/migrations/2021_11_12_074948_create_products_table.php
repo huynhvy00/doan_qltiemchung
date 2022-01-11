e783f8fa1f6e3bcaf368b1325cb3b004fc86fd26
Vy <vy.huynh@blueotter.net>
2022-01-12 00:37:47 +0700

    a3

---
+++ database/migrations/2021_11_12_074948_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->string('image',255);
+            $table->text('description');
+            $table->integer('category_id');
+            $table->string('slug',255)->unique();
+            $table->integer('quantity');
+            $table->float('price');
+            $table->float('sale_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
